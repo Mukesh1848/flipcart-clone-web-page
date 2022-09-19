5c1c6d5d35c6a247957c890f5f3b58b7375d0520
Mukesh Kumar <97693278+Mukesh1848@users.noreply.github.com>
2022-09-19 10:05:51 +0530

    filpcart clone web page
    filpcart clone web page

---
+++ flipcart clone app/js/script.js
@@ -0,0 +1,16 @@
+console.log("flipcart clone app");
+
+
+// getting the element form dom 
+
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+let number = document.getElementById('number');
+let address = document.getElementById('address');
+
+console.log(email,number,address,name);
+
+// let submitBtn = document.getElementById('submitBtn').addEventListener('click',function(){
+//         console.log('you clicked submit btn');
+//         console.log(name,email,number,address);
+// })
